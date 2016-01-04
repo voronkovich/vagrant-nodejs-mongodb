475e643b14b2a5e43d600b662362d9480507cc2b
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2016-01-05 02:15:09 +0300

    Add nodemon support

--- Vagrantfile
+++ Vagrantfile
@@ -1,14 +1,13 @@
-# -*- mode: ruby -*-
-# vi: set ft=ruby :
-
 Vagrant.configure("2") do |config|
     # You can find more Ubuntu boxes here: https://vagrantcloud.com/ubuntu
     config.vm.box = "ubuntu/trusty64"
-    config.vm.network "forwarded_port", guest: 3000, host: 3000
-    config.vm.synced_folder "app", "/home/vagrant/app"
+    config.vm.network :forwarded_port, guest: 3000, host: 3000
     config.vm.provision :shell, :path => "bootstrap.sh"
 
     config.vm.provider :virtualbox do |vb|
-        vb.customize ["modifyvm", :id, "--memory", "512"]
+        vb.memory = 512
     end
 end
+
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 68576280
-sudo apt-add-repository "deb https://deb.nodesource.com/node_5.x $(lsb_release -sc) main"
+apt-key adv --keyserver keyserver.ubuntu.com --recv 68576280
+apt-add-repository "deb https://deb.nodesource.com/node_5.x $(lsb_release -sc) main"
 
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv EA312927
-sudo apt-add-repository "deb http://repo.mongodb.org/apt/ubuntu $(lsb_release -sc)/mongodb-org/stable multiverse"
+apt-key adv --keyserver keyserver.ubuntu.com --recv EA312927
+apt-add-repository "deb http://repo.mongodb.org/apt/ubuntu $(lsb_release -sc)/mongodb-org/stable multiverse"
 
-sudo apt-get -y update
-sudo apt-get install -y nodejs
-sudo apt-get install -y mongodb-org
+apt-get -y update
+apt-get install -y nodejs
+apt-get install -y mongodb-org
 
-nodejs /home/vagrant/app/app.js &
+# Fixing npm permissions. See https://docs.npmjs.com/getting-started/fixing-npm-permissions
+sudo -iu vagrant -g vagrant <<EOF
+mkdir ~/.npm-global;
+npm config set prefix ~/.npm-global;
+echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.profile
+EOF
+
+sudo -iu vagrant -g vagrant <<EOF
+npm install -g nodemon;
+nodemon -L /vagrant/app.js &
+EOF
